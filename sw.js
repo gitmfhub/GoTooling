aca34dc16096848b3e17a239bfd0edec1227ade3
Mahmoud Fouda <147770935+gitmfhub@users.noreply.github.com>
2026-07-17 16:49:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,3 @@
-const CACHE_NAME = 'gotooling-v1';
-const urlsToCache = [
-  '/',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', function(event) {
+  // هذا ملف وسيط فارغ لمتطلبات الـ PWA
 });
